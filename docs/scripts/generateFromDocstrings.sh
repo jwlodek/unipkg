4a474d86c580c3345325e16cffc2b93e5f97df90
jwlodek <jakub.wlodek@stonybrook.edu>
2020-02-26 18:12:09 -0500

    Add some doc generation scripts

---
+++ docs/scripts/generateFromDocstrings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Simple bash script for generating markdown documentation from docstrings
+
+if [ ! -d "npdoc2md" ]
+then
+git clone https://github.com/jwlodek/npdoc2md
+fi
+cd npdoc2md
+python3 npdoc2md.py ../../../py_cui ../../DocstringGenerated -i __main__.py
