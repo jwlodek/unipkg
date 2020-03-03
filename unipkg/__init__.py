d6ad85516d750aae2a7a5418024d67ada5905af5
Jakub Wlodek <jwlodek.dev@gmail.com>
2020-03-03 15:36:55 -0500

    Can now install pip packages, need to add listing existing ones

--- unipkg/__init__.py
+++ unipkg/__init__.py
@@ -32,74 +32,185 @@ class UniPkgManager:
 
     def __init__(self, root : py_cui.PyCUI):
         self.root = root
-        self.marked_for_installation = []
-        self.marked_for_removal = []
         self.current_status = 'Install'
 
+        self.marked_packages = []
+        self.opened_packages = {}
+
+        self.passwd = None
         self.stdout_ret = None
         self.err_ret = 0
 
-        self.package_manager_selecter = self.root.add_scroll_menu('Select Package Manager', 0, 0, row_span=2, column_span=2)
+        self.package_manager_selecter = self.root.add_scroll_menu('Managers', 0, 0, row_span=2, column_span=1)
         self.package_manager_selecter.add_item_list(find_supported_package_managers())
+        self.active_package_manager = self.package_manager_selecter.view_items[self.package_manager_selecter.selected_item][3:]
+        self.package_manager_selecter.view_items[self.package_manager_selecter.selected_item] = f'-> {self.active_package_manager}'
+        self.package_manager_selecter.add_key_command(py_cui.keys.KEY_ENTER, self.select_package_manager)
 
-        self.select_operation = self.root.add_scroll_menu('Select Operation', 2, 0, row_span=2, column_span=2)
-        self.log = self.root.add_text_block('Log', 4, 0, row_span=3, column_span=2)
 
-        self.package_selection = self.root.add_checkbox_menu(f'Packages to {self.current_status}', 0, 2, row_span=6, column_span=5)
+        self.package_selection = self.root.add_checkbox_menu(f'{self.active_package_manager} Packages', 0, 1, row_span=7, column_span=6)
         self.package_selection.add_key_command(py_cui.keys.KEY_S_LOWER, self.ask_search_key)
+        self.package_selection.add_key_command(py_cui.keys.KEY_L_LOWER, self.list_packages)
+        self.package_selection.add_key_command(py_cui.keys.KEY_ENTER, self.mark_package)
 
-        operations = ['Install', 'Uninstall', 'Autoremove', 'Upgrade']
-        self.select_operation.add_item_list(operations)
+        self.marked_package_list = self.root.add_scroll_menu('Marked', 2, 0, row_span=2)
+        #self.marked_packages.add_key_command(py_cui.keys.KEY_ENTER, self.unmark_package)
 
+        self.apply_button = self.root.add_button('Apply', 4, 0, command=self.apply)
 
-        self.apply_button = self.root.add_button('Apply', 6, 2, command=exit)
-
-        self.exit_button = self.root.add_button('Exit', 6, 3, command=exit)
+        self.exit_button = self.root.add_button('Exit', 5, 0, command=exit)
 
         self.root.add_key_command(py_cui.keys.KEY_S_LOWER, self.ask_search_key)
         #self.root.add_key_command(py_cui.keys.KEY_L_LOWER, self.list_installed)
 
 
 
-    def check_admin_status(self) -> bool:
-        try:
-            is_admin = os.getuid() == 0
-        except AttributeError:
-            is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-        return is_admin
+    def mark_package(self):
+        
+        mark_toggle_pkg = None
+        for pkg in self.opened_packages.values():
+            if f'{pkg}' == self.package_selection.view_items[self.package_selection.selected_item][6:]:
+                mark_toggle_pkg = pkg
+                break
+
+        rmpkg = None
+        for pkg in self.marked_packages:
+            if pkg.name == mark_toggle_pkg.name:
+                rmpkg = pkg
+                break
+        if rmpkg is not None:
+            self.marked_packages.remove(rmpkg)
+
+        if f'{mark_toggle_pkg}' not in self.package_selection.get() and mark_toggle_pkg.installed is False:
+            self.opened_packages[mark_toggle_pkg.name].marked_op = 'Install'
+            self.marked_packages.append(self.opened_packages[mark_toggle_pkg.name])
+        elif f'{mark_toggle_pkg}' in self.package_selection.get() and mark_toggle_pkg.installed:
+            self.opened_packages[mark_toggle_pkg.name].marked_op = 'Uninstall'
+            self.marked_packages.append(self.opened_packages[mark_toggle_pkg.name])
+
+        self.refresh_marked_list()
+
+
+    def refresh_marked_list(self):
+
+        self.marked_package_list.clear()
+        for pkg in self.marked_packages:
+            self.marked_package_list.add_item(f'{pkg.name} - {pkg.marked_op}')
+
+
+
+    def select_package_manager(self) -> None:
+        self.marked_packages.clear()
+        self.opened_packages.clear()
+        self.refresh_marked_list()
+        self.active_package_manager = self.package_manager_selecter.view_items[self.package_manager_selecter.selected_item][3:]
+        for i in range(0, len(self.package_manager_selecter.view_items)):
+            if self.package_manager_selecter.view_items[i].startswith('-> '):
+                self.package_manager_selecter.view_items[i] = f'   {self.package_manager_selecter.view_items[i][3:]}'
+        self.package_manager_selecter.view_items[self.package_manager_selecter.selected_item] = f'-> {self.active_package_manager}'
+        self.package_selection.title = f'{self.active_package_manager} Packages'
+
 
     def ask_search_key(self) -> None:
         self.root.show_text_box_popup('Enter a Search Key', self.search_to_install)
+
 
     def search_to_install(self, search_key):
 
         op_thread = threading.Thread(target=lambda : self.search_to_install_op(search_key))
         op_thread.start()
-        self.root.show_loading_icon_popup('Searching', f'Fetching {self.package_manager_selecter.get()} package info')
+        self.root.show_loading_icon_popup('Searching', f'Fetching {self.active_package_manager} package info')
 
 
     def search_to_install_op(self, search_key):
-        current_package_manager = supported_package_managers[self.package_manager_selecter.get()]
-        results, installed, err = current_package_manager.search_for_packages(search_key)
+        current_package_manager = supported_package_managers[self.active_package_manager]
+        packages, err = current_package_manager.search_for_packages(search_key)
         self.root.stop_loading_popup()
-        if results is None:
+        if packages is None:
             self.root.show_error_popup('Failed to Search', 'Unable to search for packages, check network settings.')
-        elif len(results) == 0:
+        elif len(packages) == 0:
             self.root.show_warning_popup('No Results', f'No packages were found for search key {search_key}')
         else:
-            self.package_selection.clear()
-            self.package_selection.add_item_list(results)
-            for pkg in results:
-                if pkg in installed:
-                    self.package_selection.mark_item_as_checked(pkg)
-            self.root.move_focus(self.package_selection)
+
+            self.update_package_selection_list(packages)
+
+    def update_package_selection_list(self, packages):
+        self.opened_packages.clear()
+        self.package_selection.clear()
+        for pkg in packages:
+            self.opened_packages[pkg.name] = pkg
+            self.package_selection.add_item(f'{pkg}')
+            if pkg.installed:
+                self.package_selection.mark_item_as_checked(f'{pkg}')
+        self.root.move_focus(self.package_selection)
+
+
+    def list_packages(self):
+        op_thread = threading.Thread(target=self.list_packages_op)
+        op_thread.start()
+        self.root.show_loading_icon_popup('Searching', f'Locating {self.active_package_manager} installed packages')
+
+    def list_packages_op(self):
+        current_package_manager = supported_package_managers[self.active_package_manager]
+        packages, err = current_package_manager.list_packages()
+        self.root.stop_loading_popup()
+        if packages is None:
+            self.root.show_error_popup('Failed to Search', 'Unable to locate for packages!')
+        elif len(packages) == 0:
+            self.root.show_warning_popup('No Results', 'No packages were found on local system.')
+        else:
+            self.update_package_selection_list(packages)
+
+
+    def apply(self):
+
+        if len(self.marked_packages) == 0:
+            self.root.show_warning_popup('No Packages Selected', 'No packages were selected for install/uninstall!')
+            return
+
+        self.root.show_loading_bar_popup('Applying...', len(self.marked_packages))
+        op_thread = threading.Thread(target=self.apply_op)
+        op_thread.start()
+
+
+    def apply_op(self):
+        try:
+            self.root.popup.title = 'Hello World'
+            current_manager = supported_package_managers[self.active_package_manager]
+
+            for pkg in self.marked_packages:
+                self.root.popup.title = f'{pkg.marked_op}ing {pkg.name}...'
+                current_manager.install_package(pkg, self.passwd)
+                self.root.increment_loading_bar()
+
+            count = len(self.marked_packages)
+            self.opened_packages.clear()
+            self.marked_packages.clear()
+            self.refresh_marked_list()
+
+            self.root.stop_loading_popup()
+            self.root.show_message_popup('Finished Applying', f'Performed {count} package operations succesffully.')
+        except Exception as e:
+            self.root.stop_loading_popup()
+            self.root.show_error_popup('Apply Failed', 'Applying specified packages failed due to:')
+
+
 
 def find_supported_package_managers() -> List[unipkg.data_model.PackageManager]:
     ret = []
     for name in supported_package_managers.keys():
         if supported_package_managers[name].check_exists():
-            ret.append(name)
+            ret.append(f'   {name}')
     return ret
+
+
+
+def check_admin_status() -> bool:
+    try:
+        is_admin = os.getuid() == 0
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin
 
 
 
@@ -109,7 +220,11 @@ def parse_args():
 
 def main():
     root = py_cui.PyCUI(7, 7)
-    root.set_title(f'UniPkg {__version__}')
+    is_admin = check_admin_status()
+    if is_admin:
+        root.set_title(f'UniPkg {__version__} - Administrator')
+    else:
+        root.set_title(f'UniPkg {__version__}')
     root.toggle_unicode_borders()
     manager = UniPkgManager(root)
     root.start()
--- unipkg/data_model.py
+++ unipkg/data_model.py
@@ -10,11 +10,15 @@ from typing import List
 
 class Package:
 
-    def __init__(self, name: str, version: str, description: str):
+    def __init__(self, name : str, version : str, description : str, installed : bool):
         self.name = name
         self.version = version
         self.description = description
+        self.installed = installed
+        self.marked_op = None
 
+    def __format__(self, fmt : str) -> str:
+        return f'{self.name:<16} - {self.version:<8} - {self.description}'
 
 
 
@@ -27,14 +31,20 @@ class PackageManager:
     def search_for_packages(self, search_key: str):
         pass
 
-    def install_package(self, package_names, password) -> None:
-        command_str = f'{self.name} install '
-        for package in package_names:
-            command_str = command_str + package
-        return EXE.execute_command(command_str, True, passwd=password, expect='Password:')
 
-    def remove_package(self, package_names, credentials) -> None:
+    def list_packages(self) -> (List[Package], int):
         pass
+
+
+    def install_package(self, package, password, as_admin=False) -> None:
+        command_str = f'{self.name} install {package.name}'
+        return EXE.execute_command(command_str, as_admin, passwd=password, expect='Password:')
+
+
+
+    def remove_package(self, package, password, as_admin=False) -> None:
+        command_str = f'{self.name} uninstall {package.name}'
+        return EXE.execute_command(command_str, as_admin, passwd=password, expect='Password:')
 
     def check_exists(self) -> bool:
         command = f'{self.name} --version'
@@ -63,21 +73,43 @@ class Pip(PackageManager):
         super().__init__(name)
 
 
+    def list_packages(self) -> (List[Package], int):
+        command_str = f'{self.name} list'
+        out, err = EXE.execute_command(command_str, False)
+        if err !=0:
+            return None, err
+        else:
+            lines = out.splitlines()
+            packages = []
+            for line in lines:
+                if len(line) > 0:
+                    try:
+                        name_ver = line.strip().split('==')
+                        packages.append(Package(name_ver[0], name_ver[1], '', True))
+                    except:
+                        pass
+            return packages, err
+
+
     def search_for_packages(self, search_key : str) -> List[str]:
         command_str = f'{self.name} search {search_key}'
         out, err = EXE.execute_command(command_str, False)
         if err != 0:
-            return None, None, err
+            return None, err
         else:
             lines = out.splitlines()
-            ret = []
-            installed = []
+            packages = []
             for line in lines:
-                if '-' in line:
-                    ret.append(line.strip())
-                else:
-                    installed.append(ret[len(ret) - 1])
-            return ret, installed, 0
+                if len(line.strip()) == 0:
+                    pass
+                elif 'INSTALLED' in line:
+                    packages[len(packages) - 1].installed = True
+                elif '-' in line:
+                    name = line.split(' ')[0].strip()
+                    version = line.split(' ')[1].strip()[1:-1]
+                    description = line.split(')', 1)[-1].strip()[2:]
+                    packages.append(Package(name, version, description, False))
+            return packages, 0
 
 
 
